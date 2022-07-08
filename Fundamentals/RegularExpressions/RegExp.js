a7e4b006e1b8971c83cb57d85c1265aab3824c29
Sammy Mureithi <sammymureithi20@gmail.com>
2022-07-08 06:56:31 +0300

    Regex Project

--- Fundamentals/RegularExpressions/RegExp.js
+++ Fundamentals/RegularExpressions/RegExp.js
@@ -1,4 +1,6 @@
-const sentence="Hello am a ninja,as you know ninja lllove Food,a ninja is a Happy animal,happy ginjall";
+const sentence="Hello am a ninja,as you know ninja llove Food,a ninja is a Happy animal,happy ginjall";
+const sent5="Hello";
+const sent6="Hello1";
 
 const regex=/[ng]inja/g;
 
@@ -19,3 +21,13 @@ console.log(sentence.replace(rangeRepeatRangeRegExp,"7070"));
 const filter=/l+/g;
 
 console.log(sentence.replace(filter,'L'))
+
+//we are trying to match only 5 word character nothing more nothing less
+
+const fiveWord=/^[a-z]{5}$/ig;
+
+console.log(sent5.replace(fiveWord,"00000"));
+
+//let's check if this would work for a 6 character word
+console.log(sent6.replace(fiveWord,"11111"));
+//The above does not replace since it does not meet our regex condition
